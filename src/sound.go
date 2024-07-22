bdc8f980fd3d86715ba53a66df1f8fbe9757c693
PotS <potsmugen@users.noreply.github.com>
2024-07-22 16:39:42 +0100

    merge

--- src/sound.go
+++ src/sound.go
@@ -189,7 +189,7 @@ func (bgm *Bgm) Open(filename string, loop, bgmVolume, bgmLoopStart, bgmLoopEnd,
 
 	f, err := os.Open(bgm.filename)
 	if err != nil {
-		sys.bgm = *newBgm()
+		// sys.bgm = *newBgm()
 		sys.errLog.Printf("Failed to open bgm: %v", err)
 		return
 	}
