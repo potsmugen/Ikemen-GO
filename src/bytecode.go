c72ce24a2087ba3a28fa13d5a589bd0fee62c4eb
PotS <potsmugen@users.noreply.github.com>
2025-07-26 20:48:35 +0100

    feat: HitOverride guardflag.not parameter
    - Works with opposite logic from guardflag. Overrides attacks without the flags specified

--- src/bytecode.go
+++ src/bytecode.go
@@ -9414,6 +9414,7 @@ const (
 	hitOverride_forceair
 	hitOverride_forceguard
 	hitOverride_guardflag
+	hitOverride_guardflag_not
 	hitOverride_keepstate
 	hitOverride_redirectid
 )
@@ -9423,6 +9424,7 @@ func (sc hitOverride) Run(c *Char, _ []int32) bool {
 	var at, sl, st, t int32 = 0, 0, -1, 1
 	var fa, fg, ks bool
 	gf := IErr
+	gfn := IErr
 
 	StateControllerBase(sc).run(c, func(paramID byte, exp []BytecodeExp) bool {
 		switch paramID {
@@ -9448,6 +9450,8 @@ func (sc hitOverride) Run(c *Char, _ []int32) bool {
 			ks = exp[0].evalB(c) // Shouldn't be used together with StateNo but no need to block it either
 		case hitOverride_guardflag:
 			gf = exp[0].evalI(c)
+		case hitOverride_guardflag_not:
+			gfn = exp[0].evalI(c)
 		case hitOverride_redirectid:
 			if rid := sys.playerID(exp[0].evalI(c)); rid != nil {
 				crun = rid
@@ -9471,6 +9475,7 @@ func (sc hitOverride) Run(c *Char, _ []int32) bool {
 		forceguard: fg,
 		keepState: ks,
 		guardflag: gf,
+		guardflag_not: gfn,
 		playerNo: pn, // This seems to be unused currently
 	}
 	return false
