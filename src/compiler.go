0145147899bb4f834774f0622faad7c42f88286c
PotS <potsmugen@users.noreply.github.com>
2026-07-25 17:58:09 +0100

    feat: dummy Clsn type
    - Allows performing Clsn operations without interfering with boxes that already have designated purposes

--- src/compiler.go
+++ src/compiler.go
@@ -1925,6 +1925,8 @@ func (c *CharCompiler) expValue(out *BytecodeExp, in *string,
 			bv1 = BytecodeInt(2)
 		case "size":
 			bv1 = BytecodeInt(3)
+		case "dummy":
+			bv1 = BytecodeInt(4)
 		default:
 			return bvNone(), Error("Invalid collision box type: " + c1type)
 		}
@@ -1976,6 +1978,8 @@ func (c *CharCompiler) expValue(out *BytecodeExp, in *string,
 			bv1 = BytecodeInt(2)
 		case "size":
 			bv1 = BytecodeInt(3)
+		case "dummy":
+			bv1 = BytecodeInt(4)
 		default:
 			return bvNone(), Error(fmt.Sprintf("Invalid Clsn type: %s", ctype))
 		}
@@ -6531,6 +6535,8 @@ func (c *CharCompiler) paramClsnType(is IniSection, sc *StateControllerBase, par
 			box = 2
 		case "size":
 			box = 3
+		case "dummy":
+			box = 4
 		default:
 			return Error("Invalid Clsn type for " + paramName + ": " + data)
 		}
