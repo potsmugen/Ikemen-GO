7a4269029933f7d67cbf490b91b80aaeede6858f
PotS <potsmugen@users.noreply.github.com>
2026-08-15 19:03:57 +0100

    feat(trigger): StageBGVar(animloopcount)
    - Returns how many times the BG's animation has looped

--- src/script.go
+++ src/script.go
@@ -10627,6 +10627,8 @@ func triggerFunctions(l *lua.LState) {
 			switch strings.ToLower(vname) {
 			case "actionno":
 				ln = lua.LNumber(bg.actionno)
+			case "animloopcount":
+				ln = lua.LNumber(bg.anim.loopcount)
 			case "animtime":
 				ln = lua.LNumber(bg.anim.AnimTime())
 			case "delta.x":
