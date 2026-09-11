888220e6e142eb2f94f6f044f1f9d5b090841713
PotS <potsmugen@users.noreply.github.com>
2026-09-11 17:29:32 +0100

    feat(trigger): BGMVar and SoundVar sampleRate
    - Returns the sample rate ("frequency") of the sound/music

--- src/script.go
+++ src/script.go
@@ -8065,6 +8065,8 @@ func triggerFunctions(l *lua.LState) {
 					}
 				case "position":
 					ln = lua.LNumber(int32(sys.bgm.streamer.Position()))
+				case "samplerate":
+					ln = lua.LNumber(float32(sys.bgm.sampleRate))
 				case "startposition":
 					ln = lua.LNumber(int32(sys.bgm.startPos))
 				case "volume":
@@ -10389,6 +10391,8 @@ func triggerFunctions(l *lua.LState) {
 				}
 			case "priority":
 				lv = lua.LNumber(ch.sfx.priority)
+			case "samplerate":
+				lv = lua.LNumber(float32(ch.sound.format.SampleRate))
 			case "startposition":
 				lv = lua.LNumber(ch.sfx.startPos)
 			case "volumescale":
