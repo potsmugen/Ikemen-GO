6d9a18f107124c2fa0e683663d891fac5c81448b
PotS <potsmugen@users.noreply.github.com>
2026-09-15 04:02:29 +0100

    feat: maps can also store strings and ints
    - Maps can now carry a string in their values
    - They are now auto-typed
    - They can carry ints. Note: This is a breaking change. Existing char math expressions will need to be reviewed

--- src/motif.go
+++ src/motif.go
@@ -4793,8 +4793,17 @@ func (di *MotifDialogue) applyToken(m *Motif, line *DialogueParsedLine, token Di
 				return true
 			}
 			mapName, ok1 := token.value[0].(string)
-			mapVal, ok2 := token.value[1].(float32)
-			if !ok1 || !ok2 {
+			if !ok1 {
+				return false
+			}
+			// Typed from the token: numeric values stay numeric, strings are stored as text
+			var mv MapValue
+			switch v := token.value[1].(type) {
+			case float32:
+				mv = MapValue{Type: VT_Float, Num: float64(v)}
+			case string:
+				mv = MapValue{Type: VT_String, Str: v}
+			default:
 				return false
 			}
 			mapOp := int32(0)
@@ -4803,7 +4812,7 @@ func (di *MotifDialogue) applyToken(m *Motif, line *DialogueParsedLine, token Di
 					mapOp = 1
 				}
 			}
-			sys.chars[token.pn-1][0].mapSet(mapName, mapVal, mapOp)
+			sys.chars[token.pn-1][0].mapSetValue(mapName, mv, mapOp)
 		}
 		return true
 	default:
