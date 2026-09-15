00e61276e5a594daffe5d28e674bcfbf21edbba6
PotS <potsmugen@users.noreply.github.com>
2026-09-15 04:02:29 +0100

    feat: maps can also store strings and ints
    - Maps can now carry a string in their values
    - They are now auto-typed
    - They can carry ints. Note: This is a breaking change. Existing char math expressions will need to be reviewed

--- src/state.go
+++ src/state.go
@@ -550,7 +550,7 @@ type GameStatePool struct {
 	gameStatePool           sync.Pool
 	stringIntMapPool        sync.Pool
 	hitscaleMapPool         sync.Pool
-	stringFloat32MapPool    sync.Pool
+	stringMapValueMapPool   sync.Pool
 	animationTablePool      sync.Pool
 	mapArraySlicePool       sync.Pool
 	int32CharPointerMapPool sync.Pool
@@ -577,10 +577,10 @@ func NewGameStatePool() GameStatePool {
 				return &si
 			},
 		},
-		stringFloat32MapPool: sync.Pool{
+		stringMapValueMapPool: sync.Pool{
 			New: func() interface{} {
-				sf := make(map[string]float32)
-				return &sf
+				sm := make(map[string]MapValue)
+				return &sm
 			},
 		},
 		animationTablePool: sync.Pool{
@@ -645,8 +645,8 @@ func (gsp *GameStatePool) Get(item interface{}) (result interface{}) {
 	}()
 
 	switch item.(type) {
-	case (map[string]float32):
-		objs = append(objs, gsp.stringFloat32MapPool.Get())
+	case (map[string]MapValue):
+		objs = append(objs, gsp.stringMapValueMapPool.Get())
 		return objs[len(objs)-1]
 	case (map[string]int):
 		objs = append(objs, gsp.stringIntMapPool.Get())
@@ -679,8 +679,8 @@ func (gsp *GameStatePool) Get(item interface{}) (result interface{}) {
 
 func (gsp *GameStatePool) Put(item interface{}) {
 	switch item.(type) {
-	case (*map[string]float32):
-		gsp.stringFloat32MapPool.Put(item)
+	case (*map[string]MapValue):
+		gsp.stringMapValueMapPool.Put(item)
 	case (*map[string]int):
 		gsp.stringIntMapPool.Put(item)
 	case (*AnimationTable):
